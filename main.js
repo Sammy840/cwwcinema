be5a7ca9f3a8d3cc2f34caace5f5bfb2e234ddb8
Sammy840 <64469303+Sammy840@users.noreply.github.com>
2022-09-30 00:13:37 +0100

    CWW Africa

---
+++ main.js
@@ -0,0 +1,32 @@
+
+// variables for navbar menu toggle
+const header = document.querySelector('header');
+const nav = document.querySelector('nav');
+const navbarMenuBtn = document.querySelector('.navbar-menu-btn');
+
+// variables for navbar search toggle
+const navbarForm = document.querySelector('.navbar-form');
+const navbarFormCloseBtn = document.querySelector('.navbar-form-close');
+const navbarSearchBtn = document.querySelector('.navbar-search-btn');
+
+// variable for footer year
+const footerYear = document.querySelector('.footer-year');
+
+// navbar menu toggle function
+function navIsActive() {
+  header.classList.toggle('active');
+  nav.classList.toggle('active');
+  navbarMenuBtn.classList.toggle('active');
+}
+
+navbarMenuBtn.addEventListener('click', navIsActive);
+
+
+// navbar search toggle function
+const searchBarIsActive = () => navbarForm.classList.toggle('active');
+
+navbarSearchBtn.addEventListener('click', searchBarIsActive);
+navbarFormCloseBtn.addEventListener('click', searchBarIsActive);
+
+//footer year function
+footerYear.innerHTML = new Date().getFullYear();
